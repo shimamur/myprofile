4837a16900adadff51e33023e31dfacab2065975
shima <a.shimamu@icloud.com>
2021-02-14 15:29:50 +0900

    add scrollfadein

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ $(function() {
     $('.introduction').hide().fadeIn(3000);
     $('.myname').hide().fadeIn(5000);
     $('.site').hide().fadeIn(6000);
-}
-);
+
 
 // メニューバー
 
@@ -11,6 +10,7 @@ $(function() {
     $('.btnmenu').click(function(){
         $('#btnnav').toggle(300);
     });
+
 
 // 特定の位置までスクロール
 $('.menu_top').click(function(){
@@ -27,4 +27,25 @@ $('.menu_works').click(function(){
 
 $('.menu_contact').click(function(){
     $("html,body").animate({ scrollTop: $(".contact_section").offset().top });
+});
+
+
+// フェードイン
+    $(window).on('scroll load', function(){
+        let scroll_top = $(this).scrollTop();
+        let scroll_btm = scroll_top + $(this).height();
+
+        $('.fadein').each( function() {
+            let this_pos = $(this).offset().top;
+            if ( scroll_btm > this_pos ) {
+                $(this).css({
+                    opacity: 1,
+                    transform: 'translateY(0)',
+                    transform: 'translateX(0)'
+                });
+            }
+        });
+    });
+
+
 });
